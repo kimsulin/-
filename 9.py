78c963389bb6b821e0b0946e745240fd3e91473c
김수린 <83872712+kimsulin@users.noreply.github.com>
2021-05-22 21:12:15 +0900

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,11 @@
+# 1
+x = [3,2,4,1]
+
+for n in x:           # 리스트 x 를 n에 3,2,4,1
+    print(n)          # 하나씩 출력
+    
+# # 2
+x = [3,2,4,1]
+y = ["Hello", "There"]
+for c in y:           # 리스트 y를 c에 차례로
+    print(c)          # 출력된다.
