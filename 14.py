8bec39aedd0984f4d4aff05fbf496f97c3e5aeab
김수린 <83872712+kimsulin@users.noreply.github.com>
2021-06-07 19:50:02 +0900

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,11 @@
+print("3개의 값을 입력하시오.")
+x = int(input("x 값을 입력해라.:"))
+y = int(input("y 값을 입력해라.:"))
+z = int(input("z 값을 입력해라.:"))
+x = x + 1
+y = y + 1
+z = z + 1
+print("변경된 x 값: ", x)
+print("변경된 y 값: ", y)
+print("변경된 z 값: ", z) 
+print("변경된 값: ", x, y, z) 
